ad5edb9685f7b40537520005770b7c25e74f2688
chris <cfrangoudes@gmail.com>
2018-07-10 18:54:44 +0300

    1. Added onRestart to Activity 2. Added more Toast messages 3. Added message to Intent passed to the Service 4. Added onCreate to Service 5. Added onUnbind to Service
    Commiting before experimenting in preparation for creating a Service MyAudioPlayer

--- app/src/main/java/com/frangoudes/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/frangoudes/activitylifecycle/MainActivity.java
@@ -1,37 +1,30 @@
 package com.frangoudes.activitylifecycle;
 
-// Based on tutorial at https://www.tutorialspoint.com/android/index.htm
+// Based on tutorial at https://www.tutorialspoint.com/android/android_acitivities.htm
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
-
-/*
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
-*/
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    String msg = "Android : ";
+    String msg = "MainActivity log msg";
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onCreate() event");
     }
 
     public void startService(View view) {
-        startService(new Intent(getBaseContext(), MyService.class));
+        Intent intent= new Intent(getBaseContext(), MyService.class);
+        intent.putExtra(MyService.KEY1, "Hi");
+        startService(intent);
     }
 
     // Method to stop the service
@@ -43,13 +36,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        Log.d(msg, "The onRestart() event");
     }
 
     /** Called when the activity has become visible. */
     @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onResume() event");
     }
 
@@ -57,6 +60,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onPause() event");
     }
 
@@ -64,6 +68,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onStop() event");
     }
 
@@ -71,6 +76,7 @@ public class MainActivity extends Activity {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
         Log.d(msg, "The onDestroy() event");
     }
 }
--- app/src/main/java/com/frangoudes/activitylifecycle/MyService.java
+++ app/src/main/java/com/frangoudes/activitylifecycle/MyService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -11,22 +12,46 @@ import android.widget.Toast;
  */
 
 public class MyService extends Service {
+
+    public static final String KEY1 = "KEY1 intent message";
+    String msg = "Service log msg";
+
     @Nullable
     @Override
+
     public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "Service Bound", Toast.LENGTH_SHORT).show();
         return null;
     }
 
     @Override
+    public void onCreate(){
+        Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(msg, "Service started");
         // Let it continue running until it is stopped.
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        String key1Msg = intent.getStringExtra(KEY1);
+        Toast.makeText(this, "Service Started with Intent Msg: "+key1Msg, Toast.LENGTH_SHORT).show();
         return START_STICKY;
     }
 
     @Override
+    public boolean onUnbind(Intent intent) {
+        return true;
+    }
+
+    /** Called when a client is binding to the service with bindService()*/
+    @Override
+    public void onRebind(Intent intent) {
+
+    }
+    @Override
     public void onDestroy() {
+        Log.d(msg, "Service stopped");
         super.onDestroy();
-        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
     }
 }
